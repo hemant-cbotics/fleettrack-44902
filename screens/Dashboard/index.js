71ab31c693dc913d07e430e19f078f21e95b877b
ShivaPandey02 <shiva.pandey@crowdbotics.com>
2023-12-26 22:44:37 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Dashboard/index.js
+++ screens/Dashboard/index.js
@@ -3,79 +3,79 @@ import React from 'react';
 import { SafeAreaView, View, Text, TextInput, Button, TouchableOpacity, ScrollView, Image } from 'react-native';
 
 const Dashboard = () => {
-  return <SafeAreaView style={_styles.PfjqlYRo}>
-      <View style={_styles.KTsuATFj}>
-        <TextInput placeholder="Search" style={_styles.CBRSrbtM} />
-        <Text style={_styles.EzaaIqOY}>Settings</Text>
+  return <SafeAreaView style={_styles.uftybcNK}>
+      <View style={_styles.AOXYQesl}>
+        <TextInput placeholder="Search" style={_styles.ZQwwTfbF} />
+        <Text style={_styles.QwLqfUPL}>Settings</Text>
         <Button title="Set number of visible tiles" onPress={() => {}} />
         <Button title="Show/Hide Tiles" onPress={() => {}} />
         <Button title="Set Dashboard Tiles Location" onPress={() => {}} />
         <Button title="Create New Dashboard" onPress={() => {}} />
       </View>
       <ScrollView>
-        <View style={_styles.jLKeWaHz}>
-          <Text style={_styles.tVqFGLZq}>Dashboard Tiles</Text>
-          <View style={_styles.HkBEPYYJ}>
-            <Text style={_styles.NIhINqmi}>Driver and Device Overview</Text>
+        <View style={_styles.CwnIqQWz}>
+          <Text style={_styles.WWPcYQAY}>Dashboard Tiles</Text>
+          <View style={_styles.WTNhtZap}>
+            <Text style={_styles.GhovysNw}>Driver and Device Overview</Text>
             <Text>Current Locations and Status</Text>
             <Image source={{
             uri: 'https://tinyurl.com/42evm3m3'
-          }} style={_styles.wgHxbYJd} />
+          }} style={_styles.RDOsTXFe} />
             <Button title="Filter Options" onPress={() => {}} />
             <Text>Real-time map showing the current locations of drivers and devices</Text>
             <Text>Quick status indicators for each driver and device</Text>
           </View>
-          <View style={_styles.TLmpRbSk}>
-            <Text style={_styles.RVtSqbgu}>Safety Insights</Text>
+          <View style={_styles.ANDWNJKr}>
+            <Text style={_styles.GcYxNstq}>Safety Insights</Text>
             <Text>Safety Metrics</Text>
             <Image source={{
             uri: 'https://tinyurl.com/42evm3m3'
-          }} style={_styles.qOBxoXPE} />
+          }} style={_styles.sPxqNOWu} />
             <Button title="Filter Options" onPress={() => {}} />
             <Text>Graphs highlighting safety-critical events</Text>
             <Text>Coaching recommendations to improve driver safety</Text>
           </View>
-          <View style={_styles.HMGRjHJJ}>
-            <Text style={_styles.RgmRFUXR}>Compliance Snapshot</Text>
+          <View style={_styles.anWuCGmF}>
+            <Text style={_styles.wjvhGlXL}>Compliance Snapshot</Text>
             <Text>Compliance Overview</Text>
             <Image source={{
             uri: 'https://tinyurl.com/42evm3m3'
-          }} style={_styles.jemsABzx} />
+          }} style={_styles.npHjItyN} />
             <Button title="Filter Options" onPress={() => {}} />
             <Text>ELD status and hours of service compliance</Text>
             <Text>Adherence to company policies</Text>
           </View>
-          <View style={_styles.DdRTxngY}>
-            <Text style={_styles.gIyvoJaX}>Maintenance Overview</Text>
+          <View style={_styles.EpviSVnD}>
+            <Text style={_styles.lIJOVDRA}>Maintenance Overview</Text>
             <Text>Vehicle Maintenance</Text>
             <Image source={{
             uri: 'https://tinyurl.com/42evm3m3'
-          }} style={_styles.EgSsPoOI} />
+          }} style={_styles.NzWBDdPJ} />
             <Button title="Filter Options" onPress={() => {}} />
             <Text>Preventative maintenance schedule</Text>
             <Text>DVIR status and logs</Text>
             <Text>Real-time vehicle status</Text>
           </View>
-          <View style={_styles.sXleENbv}>
-            <Text style={_styles.rkTajAsb}>Fuel and Energy Usage</Text>
+          <View style={_styles.freqfPLB}>
+            <Text style={_styles.kYCPNaGZ}>Fuel and Energy Usage</Text>
             <Text>Fuel and Energy Management</Text>
             <Image source={{
             uri: 'https://tinyurl.com/42evm3m3'
-          }} style={_styles.XIzDfXjk} />
+          }} style={_styles.SnDugVhd} />
             <Button title="Filter Options" onPress={() => {}} />
             <Text>Fuel tax information</Text>
             <Text>Fuel spend and usage for drivers and devices</Text>
           </View>
-          <View style={_styles.xfshGKGO}>
-            <Text style={_styles.dBUqQliy}>Custom Reports</Text>
+          <View style={_styles.pSKIjFMi}>
+            <Text style={_styles.vUJoQMZM}>Custom Reports</Text>
             <Text>My Reports</Text>
             <Image source={{
             uri: 'https://tinyurl.com/42evm3m3'
-          }} style={_styles.JqLiGHZR} />
+          }} style={_styles.JfyeqjwJ} />
             <Button title="Filter Options" onPress={() => {}} />
             <Text>A condensed view of custom reports</Text>
             <TouchableOpacity onPress={() => {}}>
-              <Text style={_styles.DfOxBybb}>Create and customize new reports</Text>
+              <Text style={_styles.ocVkWlAq}>Create and customize new reports</Text>
             </TouchableOpacity>
           </View>
         </View>
@@ -86,121 +86,121 @@ const Dashboard = () => {
 export default Dashboard;
 
 const _styles = StyleSheet.create({
-  PfjqlYRo: {
+  uftybcNK: {
     flex: 1,
     backgroundColor: "#f5f5f5"
   },
-  KTsuATFj: {
+  AOXYQesl: {
     padding: 20
   },
-  CBRSrbtM: {
+  ZQwwTfbF: {
     backgroundColor: "#fff",
     borderRadius: 10,
     padding: 10
   },
-  EzaaIqOY: {
+  QwLqfUPL: {
     fontSize: 20,
     fontWeight: "bold",
     marginTop: 20
   },
-  jLKeWaHz: {
+  CwnIqQWz: {
     padding: 20
   },
-  tVqFGLZq: {
+  WWPcYQAY: {
     fontSize: 20,
     fontWeight: "bold"
   },
-  HkBEPYYJ: {
+  WTNhtZap: {
     marginTop: 20,
     backgroundColor: "#fff",
     borderRadius: 10,
     padding: 20
   },
-  NIhINqmi: {
+  GhovysNw: {
     fontSize: 18,
     fontWeight: "bold"
   },
-  wgHxbYJd: {
+  RDOsTXFe: {
     width: "100%",
     height: 200,
     marginTop: 10
   },
-  TLmpRbSk: {
+  ANDWNJKr: {
     marginTop: 20,
     backgroundColor: "#fff",
     borderRadius: 10,
     padding: 20
   },
-  RVtSqbgu: {
+  GcYxNstq: {
     fontSize: 18,
     fontWeight: "bold"
   },
-  qOBxoXPE: {
+  sPxqNOWu: {
     width: "100%",
     height: 200,
     marginTop: 10
   },
-  HMGRjHJJ: {
+  anWuCGmF: {
     marginTop: 20,
     backgroundColor: "#fff",
     borderRadius: 10,
     padding: 20
   },
-  RgmRFUXR: {
+  wjvhGlXL: {
     fontSize: 18,
     fontWeight: "bold"
   },
-  jemsABzx: {
+  npHjItyN: {
     width: "100%",
     height: 200,
     marginTop: 10
   },
-  DdRTxngY: {
+  EpviSVnD: {
     marginTop: 20,
     backgroundColor: "#fff",
     borderRadius: 10,
     padding: 20
   },
-  gIyvoJaX: {
+  lIJOVDRA: {
     fontSize: 18,
     fontWeight: "bold"
   },
-  EgSsPoOI: {
+  NzWBDdPJ: {
     width: "100%",
     height: 200,
     marginTop: 10
   },
-  sXleENbv: {
+  freqfPLB: {
     marginTop: 20,
     backgroundColor: "#fff",
     borderRadius: 10,
     padding: 20
   },
-  rkTajAsb: {
+  kYCPNaGZ: {
     fontSize: 18,
     fontWeight: "bold"
   },
-  XIzDfXjk: {
+  SnDugVhd: {
     width: "100%",
     height: 200,
     marginTop: 10
   },
-  xfshGKGO: {
+  pSKIjFMi: {
     marginTop: 20,
     backgroundColor: "#fff",
     borderRadius: 10,
     padding: 20
   },
-  dBUqQliy: {
+  vUJoQMZM: {
     fontSize: 18,
     fontWeight: "bold"
   },
-  JqLiGHZR: {
+  JfyeqjwJ: {
     width: "100%",
     height: 200,
     marginTop: 10
   },
-  DfOxBybb: {
+  ocVkWlAq: {
     color: "blue"
   }
 });
