22a6d14cbe4789a43b02a6906419a5641efffc0f
Prince Yadav <prince.yadav@crowdbotics.com>
2024-05-13 18:52:52 +0530

    Fix bug in form field of admin

--- web-app/src/components/admin/formFields.tsx
+++ web-app/src/components/admin/formFields.tsx
@@ -8,7 +8,7 @@ type AdminFormFieldInputProps = {
   placeholder?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
-  value?: string;
+  value?: any;
   touched?: boolean;
   error?: string;
   disabled?: boolean;
@@ -176,10 +176,12 @@ export const AdminFormFieldCheckbox: FC<AdminFormFieldInputProps> = ({
   value,
   onChange,
   onBlur,
+  touched,
+  error,
   disabled = false,
 }) => {
 
-  const [checked, setChecked] = useState(value === "true" ? true : false);
+  const [checked, setChecked] = useState(value);
 
   const handleChange = (e:any) => {
     setChecked(e.target.checked);
@@ -204,7 +206,7 @@ export const AdminFormFieldCheckbox: FC<AdminFormFieldInputProps> = ({
           onBlur={onBlur}
           className="mt-1 w-full h-11 px-3 rounded-md text-sm shadow-sm sr-only peer focus-visible:outline-4 focus-visible:shadow-none"
         />
-        <div onClick={() => disabled ? () => {} : handleChange({ target: { checked: !checked, value: !checked ? 'true' : 'false', name: name } })} className="relative w-12 h-6 bg-gray-200 rounded-full peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-500"></div>
+        <div onClick={() => disabled ? () => {} : handleChange({ target: { checked: !checked, value: !checked ? true : false, name: name } })} className="relative w-12 h-6 bg-gray-200 rounded-full peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-500"></div>
       </div>
     </div>
   );
