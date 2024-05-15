f5e6163ede7303e9d4e3452b8122eafc6bc126ec
Prince Yadav <prince.yadav@crowdbotics.com>
2024-05-15 14:53:11 +0530

    Fix the keys of initial values

--- web-app/src/pages/admin-details/vehicle/validation.ts
+++ web-app/src/pages/admin-details/vehicle/validation.ts
@@ -1,6 +1,6 @@
 import * as Yup from "yup";
 
-export const vehicleDetailFormInitialValues = {
+export const vehicleFormInitialValues = {
   vehicle_id: "",
   creation_date: "",
   server_id: "",
@@ -8,7 +8,7 @@ export const vehicleDetailFormInitialValues = {
   unique_id: "",
   previous_unique_id: "",
   unique_id_last_change: "",
-  is_active: "",
+  is_active: false,
   vehicle_description: "",
   short_name: "",
   vin: "",
@@ -22,48 +22,28 @@ export const vehicleDetailFormInitialValues = {
   vehicle_class: "",
   imei_number: "",
   serial_number: "",
-  country_code: "",
   phone_number: "",
   email: "",
   group_pushpin_id: "",
   map_route_color: "",
   ignition_input: "",
   maximum_speed: "",
-  driver_name: "",
-  driver_country_code: "",
-  driver_phone_number: "",
   driver_id: "",
+  driver_name: "",
+  driver_phone_number: "",
   fuel_type: "",
   fuel_capacity: "",
   fuel_economy: "",
   fuel_cost: "",
-}
-
-export const vehicleGroupMembershipFormInitialValues = {
+  recorder_id: "",
+  recorder_on: "",
+  recorder_type: "",
+  previous_recorder_id: "",
+  recorder_id_last_changed: "",
+  list_of_groups: "",
   all_vehicles: "",
-  demo_map: "",
-  district1: "",
-  district2: "",
-  district3: "",
-  district4: "",
-  district5: "",
-  district6: "",
-  district7: "",
-  district8: "",
-  group_a: "",
-  group_b: "",
-  hybrid_vehicle: "",
-  jacob: "",
-  k9_units: "",
-  map_demo: "",
-  medium_group: "",
-  meitrack: "",
-  patrol_cars: "",
-  public_works_pickups: "",
-  small_group: "",
-  snow_plows: "",
 }
 
-export const detailFormValidationSchema = Yup.object().shape({});
+export const vehicleFormValidationSchema = Yup.object().shape({});
 
 export const groupMembershipFormValidationSchema = Yup.object().shape({});
