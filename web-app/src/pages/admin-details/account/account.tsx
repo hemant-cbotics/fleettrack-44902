c18380bb4055bdd6127e364719f4cd9762712bbb
Prince Yadav <prince.yadav@crowdbotics.com>
2024-05-17 09:51:54 +0530

    remaining form of account admin and their initial values and validation

--- web-app/src/pages/admin-details/account/account.tsx
+++ web-app/src/pages/admin-details/account/account.tsx
@@ -2,25 +2,32 @@ import { useFormik } from "formik";
 import Accordian from "../../../components/accordian";
 import HeaderView from "../../../components/admin/headerView";
 import { APP_CONFIG } from "../../../constants/constants";
-import { AccountGeneralDetailForm } from "./account-form";
 import {
-  accountGeneralDetailsInitialValues,
-  accountGeneralDetailsYupValidationSchema,
+  AccountGeneralDetailForm,
+  AccountMaintenanceIntervalLabelForm,
+  AccountScorecardWeightFactorsForm,
+} from "./account-form";
+import {
+  accountDetailsInitialValues,
+  accountDetailsYupValidationSchema,
 } from "./validation";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 import { routeUrls } from "../../../navigation/routeUrls";
+import { AdminFormFieldSubmit } from "../../../components/admin/formFields";
 
 const ScreenAdminDetailAccount = () => {
   const [userCanEdit, setUserCanEdit] = useState(false);
-  const { t: tmain } = useTranslation();
-  const { t } = useTranslation('translation', { keyPrefix: 'dashboard.profile.account' });
+  const { t: tMain } = useTranslation();
+  const { t } = useTranslation("translation", {
+    keyPrefix: "dashboard.profile.account",
+  });
   const navigate = useNavigate();
 
   const formik = useFormik({
-    initialValues: accountGeneralDetailsInitialValues,
-    validationSchema: accountGeneralDetailsYupValidationSchema,
+    initialValues: accountDetailsInitialValues,
+    validationSchema: accountDetailsYupValidationSchema,
     onSubmit: (values) => {
       console.log(values);
     },
@@ -36,24 +43,44 @@ const ScreenAdminDetailAccount = () => {
 
   const handleEditInformation = () => {
     setUserCanEdit(!userCanEdit);
-  }
+  };
+
+  const isLoadingEditAccount = false;
 
   return (
     <>
-      <HeaderView title={t("heading")} showBackButton={true} backButtonCallback={() => navigate(routeUrls.dashboard)} />
+      <HeaderView
+        title={t("heading")}
+        showBackButton={true}
+        backButtonCallback={() => navigate(routeUrls.dashboard)}
+      />
       <div className={`${APP_CONFIG.DES.DASH.P_HORIZ} py-2`}>
         <div className="flex items-center font-semibold text-blue-900 text-lg leading-6">
           {t("sub_heading")}
         </div>
         <div className="px-4">
-          <div className="flex justify-end space-x-4">
-            <button className="rounded-full bg-blue-200 px-6 py-2 font-medium text-lg leading-6 text-blue-900" onClick={handleEditInformation}>
-              {userCanEdit ? tmain("update") : tmain("edit")}
-            </button>
+          <div className="flex items-center gap-4">
+            <div className="flex-grow"></div>
+            <div className="w-24">
+              <AdminFormFieldSubmit
+                type="button"
+                variant="primary"
+                label={userCanEdit ? tMain("update") : tMain("edit")}
+                onClick={
+                  userCanEdit
+                    ? handleEditInformation
+                    : () => setUserCanEdit(!userCanEdit)
+                }
+                disabled={isLoadingEditAccount}
+              />
+            </div>
           </div>
           <div className="rounded-lg mt-2 bg-blue-200">
             <form onSubmit={handleSubmit}>
-              <Accordian title={t("accord_general_details")} openByDefault={true}>
+              <Accordian
+                title={t("accord_general_details")}
+                openByDefault={true}
+              >
                 <AccountGeneralDetailForm
                   values={values}
                   errors={errors}
@@ -65,11 +92,29 @@ const ScreenAdminDetailAccount = () => {
                   userCanEdit={userCanEdit}
                 />
               </Accordian>
-              <Accordian title={t("accord_maintainance_interval_labels")}>
-                <p className="px-5 pt-4 pb-8 bg-gray-100">Information not available</p>
+              <Accordian title={t("accord_maintenance_interval_labels")}>
+                <AccountMaintenanceIntervalLabelForm
+                  values={values}
+                  errors={errors}
+                  touched={touched}
+                  handleChange={handleChange}
+                  formikSetValue={formik.setFieldValue}
+                  handleBlur={handleBlur}
+                  formikSetTouched={formik.setFieldTouched}
+                  userCanEdit={userCanEdit}
+                />
               </Accordian>
               <Accordian title={t("accord_scorecard_weight_factors")}>
-                <p className="px-5 pt-4 pb-8 bg-gray-100">Information not available</p>
+                <AccountScorecardWeightFactorsForm 
+                  values={values}
+                  errors={errors}
+                  touched={touched}
+                  handleChange={handleChange}
+                  formikSetValue={formik.setFieldValue}
+                  handleBlur={handleBlur}
+                  formikSetTouched={formik.setFieldTouched}
+                  userCanEdit={userCanEdit}
+                />
               </Accordian>
             </form>
           </div>
