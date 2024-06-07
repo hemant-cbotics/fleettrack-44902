48a364b9045ab3fe9f9d905f2368bea0a6f5f7da
Hemant Kumar <hemant.kumar@crowdbotics.com>
2024-06-07 16:55:22 +0530

    Initialized Map Overview Screen with listing and markers on Map

--- web-app/src/pages/map-overview/type.ts
+++ web-app/src/pages/map-overview/type.ts
@@ -1,3 +1,5 @@
+import { OrganizationVehicle } from "../../api/types/Vehicle";
+import { TLatLng } from "../../types/map";
 
 export type TVehicleList = {
   id: string;
@@ -13,3 +15,32 @@ export type TGroupList = {
   listOfVehicles: TVehicleList[];
   checked?: boolean;
 }
+
+export type TDataPoint = OrganizationVehicle & {
+  coords: number[]; // TODO: dissolve this type later and use OranizationVehicle's lat and lon
+}
+
+export type TMapRef = {
+  map: any;
+  objects: {
+    mPushpins: TMPushpin[];
+  }
+}
+
+export type TMPushpin = {
+  id: string;
+  pushpin: any;
+}
+
+export type TMapData = {
+  centerPosition: TLatLng;
+  radius: number;
+}
+
+export type TMapOperationsProps = {
+  mapRef: React.MutableRefObject<TMapRef>;
+  mapData: TMapData;
+  setMapData: (mapData: TMapData) => void;
+  dataPoints: TDataPoint[];
+}
+export type TMapOperations = (props: TMapOperationsProps) => void;
